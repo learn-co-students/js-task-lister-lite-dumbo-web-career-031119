a355f4e6a69d4eeca3153f491d7ef4844c984fa9
github username <itsDario1@gmail.com>
2019-04-27 08:43:09 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const processInput = inputEvent => {
+    inputEvent.preventDefault()
+    // console.log(inputEvent.target[0].value)
+    taskList.innerHTML += `<li>${inputEvent.target[0].value}</li>`
+  }
+
+
+  const taskForm = document.querySelector('#create-task-form')
+  const taskList = document.querySelector('#tasks')
+  taskForm.addEventListener('submit', processInput)
 });
