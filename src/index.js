32be3fea8470ad61db493275c25068ab5d1b76a6
siberry <siberry226@gmail.com>
2019-04-29 11:33:36 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const newTaskForm = document.getElementById("create-task-form");
+
+  newTaskForm.addEventListener("submit", createNewTask);
+
+  const tasks = document.getElementById("tasks")
+
+  tasks.addEventListener("click", () => {
+    if (event.target.nodeName === "BUTTON") {
+      event.target.parentElement.remove()
+    }
+  })
+
 });
+
+const createNewTask = event => {
+  event.preventDefault();
+  let newTaskDescription = document.getElementById("new-task-description").value;
+  const newTask = document.createElement('li');
+  const tasksList = document.getElementById("tasks");
+  const removeButton = document.createElement('button');
+
+  removeButton.innerText = "X"
+  removeButton.setAttribute("data-description", newTaskDescription);
+
+  newTask.innerText = newTaskDescription;
+  newTask.appendChild(removeButton);
+
+  tasksList.appendChild(newTask);
+
+  event.target.reset();
+
+}
